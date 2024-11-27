9fdf510209f71722417e36b0e27b444d834e2b52
Mikhail <may22@tpu.ru>
2024-11-27 17:09:45 +0700

    kabachochki fixed

--- app/src/main/java/com/tomsksmarttech/smart_alarm_mobile/MusicScreen.kt
+++ app/src/main/java/com/tomsksmarttech/smart_alarm_mobile/MusicScreen.kt
@@ -1,12 +1,76 @@
 package com.tomsksmarttech.smart_alarm_mobile
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import java.io.File
 
 
 @Composable
 fun MusicScreen() {
-    Text("This is a music screen", Modifier.fillMaxSize())
+    MusicTopAppBar()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MusicTopAppBar() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.heightIn(max = 56.dp),
+                title = { Text(stringResource(R.string.title_music), fontWeight = FontWeight.Bold) },
+                scrollBehavior = scrollBehavior,
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {TODO("Функция для кнопки поиска")}) {
+                Icon(Icons.Filled.Search, contentDescription = "Поиск музыки")
+            }
+        }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier.padding(innerPadding),
+        ) {
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MusicScreenPreview() {
+    MusicScreen()
 }
